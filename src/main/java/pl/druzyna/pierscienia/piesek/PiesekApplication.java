57b9e3d4efe02ddb9553c70b490926c0553073c2
Damian <damianwnukowski97@gmail.com>
2019-12-09 22:53:28 +0100

    Initial commit

---
+++ src/main/java/pl/druzyna/pierscienia/piesek/PiesekApplication.java
@@ -0,0 +1,13 @@
+package pl.druzyna.pierscienia.piesek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiesekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiesekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/druzyna/pierscienia/piesek/PiesekApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.druzyna.pierscienia.piesek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiesekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
